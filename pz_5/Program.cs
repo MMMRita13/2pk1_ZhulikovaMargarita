c082fb185fa7d072d67f57e3aad986b3f00aa2b4
Маргарита Жуликова <m.zhulikova05@mail.ru>
2022-09-22 09:48:31 +0500

    5  вариант

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -6,7 +6,44 @@ namespace pz_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("работа с git");
-        }
+            for (int i = 0; i <= 60; i += 5)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            for (char a = 'e'; a <= 'j'; a++)
+            {
+                Console.Write(a);
+            }
+
+
+            int n = 3;
+            int m = 6;
+            for (int i = 0; i < m; i++)
+            {
+                for (int x = 0; x < n; x++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+
+            for (int i = 2, j = 40; i - j <= 18; i++, j--)
+            {
+                Console.WriteLine(i - j);
+            }
+        }   
+
     }
 }
