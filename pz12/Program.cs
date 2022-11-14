a3c5f134d53d6af1b1979b3b2c863277e43da97f
MMMRita13 <114126777+MMMRita13@users.noreply.github.com>
2022-11-14 10:14:22 +0500

    Add files via upload

---
+++ pz12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace pz12
+{
+    internal class Program
+    {
+        static void S(double a, double b, double h1, double h2)
+        {
+            double S1 = a * h1;//площадь первого параллелограмма
+            double S2 = b * h2;//площадь второго параллелограмма
+            //сравнение площадей параллелограмма
+            if (S1 > S2)
+            {
+                Console.WriteLine(S1);
+            }
+            else if (S2 > S1)
+            {
+                Console.WriteLine(S2);
+            }
+            else if (S1 == S2)
+            {
+                Console.WriteLine("Параллелограммы равны");
+            }
+        }
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());//сторона первого параллелограмма
+            double h1 = Convert.ToDouble(Console.ReadLine());//высота первого параллелограммма
+            double b = Convert.ToDouble(Console.ReadLine());//сторона второго параллелограмма
+            double h2 = Convert.ToDouble(Console.ReadLine());//высота второго параллелограммма
+            S(a, b, h1, h2);
+        }
+    }
+}
