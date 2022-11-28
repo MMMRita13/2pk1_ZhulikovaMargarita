d971473c62d1cb49582a973419b3993fb46b912a
MMMRita13 <114126777+MMMRita13@users.noreply.github.com>
2022-11-28 12:15:53 +0500

    Add files via upload

---
+++ pz_16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace pz_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "text.txt"; //созаем путь файла
+            FileInfo text = new FileInfo(path);//создаем объект файл
+            FileStream file = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite); //поток файла
+            StreamReader read = new StreamReader(file); //поток чтения
+           
+            string maxString = "", c;
+            while ((c = read.ReadLine()) != null)//сравнение
+            {
+                if (c.Length > maxString.Length)
+                    maxString = c;
+            }
+
+            Console.WriteLine( maxString);
+            read.Close();
+        }
+    }
+}
