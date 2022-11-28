91b9254e3f0129cec422d0d1e5c5e3d5013cf2d3
MMMRita13 <114126777+MMMRita13@users.noreply.github.com>
2022-11-28 09:43:09 +0500

    Add files via upload

---
+++ pz_15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace pz_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\text.txt"; //созаем путь файла
+            FileInfo text = new FileInfo(path);//создаем файл
+            int[,] file = new int[5, 8];//двумерный массив
+            FileStream file1 = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write); //поток файла
+            StreamWriter zapic = new StreamWriter(file1); //поток записи
+            for (int i = 0; i < 5; i++)//заполняем массив и записываем в файл
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    file[i, j] = i * j;
+                    zapic.Write(file[i, j] + " ");
+                    Console.Write(file[i, j] + " ");
+                }
+                Console.WriteLine();
+                zapic.WriteLine();
+            }
+            zapic.Close();
+        }
+    }
+}
