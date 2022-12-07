0051f5ccb0c598537f94176d7f2fc2c6d53da2c8
MMMRita13 <114126777+MMMRita13@users.noreply.github.com>
2022-12-07 09:19:48 +0500

    Add files via upload

--- console.igra/Program.cs
+++ console.igra/Program.cs
@@ -4,90 +4,193 @@ namespace console.igra
 {
     internal class Program
     {
-        public static void GenerateMap(ref string[,] carta, string aptechka, string vrag, string buffer, string igrok)
+        static string[,] carta = new string[25, 25];
+        static string igrok = "@";
+        static string aptechka = "A";
+        static string vrag = "V";
+        static string buffer = "B";
+        static int buffer_B = 5;
+        static int x = 12;
+        static int y = 12;
+        static int go = 0;
+        static int igrok_HP = 30;
+        static int igrok_power = 5;
+        static int vrag_HP = 15;
+        static int vrag_power = 5;
+        static int kolvo_vrag = 10;
+        public static Array GenerateMap(ref string[,] carta, string aptechka, string vrag, string buffer, string igrok)
         {//генерация карта
             Random rdm = new Random();
+            carta[x, y] = igrok;
             for (int i = 0; i < 5; i++)
             {
-                carta[rdm.Next(0, 25), rdm.Next(0, 25)] = aptechka;
+                int a = rdm.Next(0, 25);
+                int b = rdm.Next(0, 25);
+                if (carta[a,b] == "*")
+                {
+                    carta[a,b] = aptechka;
+                }
             }
             for (int i = 0; i < 10; i++)
             {
-                carta[rdm.Next(0, 25), rdm.Next(0, 25)] = vrag;
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                carta[rdm.Next(0, 25), rdm.Next(0, 25)] = buffer;
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                carta[13, 13] = igrok;
-            }
-
-            }
-        }
-            static void Main(string[] args)
-        {
-            int HP = 30;
-            int power = 5;
-            Console.WriteLine("Здоровье = " + HP + " " + "Сила = " + power);
-            
-            string[,] carta = new string[25, 25];
-            string igrok = "@";
-            string aptechka = "A";
-            string vrag = "V";
-            string buffer = "B";
-            for (int x = 0; x < carta.GetLength(0); x++)
-            {
-                for (int y = 0; y < carta.GetLength(1); y++)
+                int a = rdm.Next(0, 25);
+                int b = rdm.Next(0, 25);
+                if (carta[a, b] == "*")
                 {
-                    Console.Write(carta[x, y] + " * ");
+                    carta[a, b] = vrag;
                 }
             }
-        GenerateMap(ref carta, aptechka, vrag, buffer, igrok);
-            for (int x = 0; x < carta.GetLength(0); x++)
+            for (int i = 0; i < 3; i++)
             {
-                for (int y = 0; y < carta.GetLength(1); y++)
+                int a = rdm.Next(0, 25);
+                int b = rdm.Next(0, 25);
+                if (carta[a, b] == "*")
                 {
-                    Console.Write(" " + carta[x, y] + "  ");
+                    carta[a, b] = buffer;
+                }
+            }
+            for (int i = 0; i < carta.GetLength(0); i++)
+            {
+                for (int j = 0; j < carta.GetLength(1); j++)
+                {
+                    Console.Write(carta[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            return (carta);
+        }
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Здоровье игрока = " + igrok_HP + " " + "Сила = " + igrok_power);
+
+             
+            for (int i = 0; i < carta.GetLength(0); i++)
+            {
+                for (int j = 0; j < carta.GetLength(1); j++)
+                {
+                    carta[i, j] = "*";
+                }
+            }
+            GenerateMap(ref carta, aptechka, vrag, buffer, igrok);
+            
+            for(int i = 0; igrok_HP > 0; i++)
+            {
+                if (kolvo_vrag>0)
+                {
+                    carta[x, y] = "*";
+                    Move(ref x, ref y);
+                    if (carta[x, y] == vrag)
+                    {
+                        Fight();
+                    }
+                    if (carta[x, y] == aptechka)
+                    {
+                        Healing();
+                    }
+                    if (carta[x, y] == buffer)
+                    {
+                         Buff();
+                    }
+                    UpdateMap();
+                    
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Игра окончена за {go} шагов");
+                    Console.WriteLine("Вы выиграли!");
+                }
+            }
+            SaveState();
+        }
+        public static Array UpdateMap()
+        {//обновление карты после действий}
+            Console.Clear();
+            carta[x, y] = igrok;
+            Console.WriteLine("Здоровье игрока"+igrok_HP + "Сила = " + igrok_power);
+
+            Console.WriteLine("Общее количество шагов:" + go);
+            if (buffer_B < 5)
+            {
+                Console.WriteLine($"Осталось {(5 - buffer_B)} шагов до конца баффа");
+            }
+            for (int i = 0; i < carta.GetLength(0); i++)
+            {
+                for (int j = 0; j < carta.GetLength(1); j++)
+                {
+                    Console.Write(carta[i, j] + "  ");
                 }
                 Console.WriteLine("");
             }
+            return (carta);
 
-            UpdateMap();
-            Move();
-            Fight();
-            Healing();
-            Buff();
-            SaveState();
         }
-        public static void UpdateMap()
-        {//обновление карты после действий}
-        }
-        public static void Move()
+        public static Array Move(ref int x, ref int y)
         {//реализация перемещения на нужную ячейку в связи с выбранным направлением direction, подсчет шагов}
-            string napravlenie = "→";
-            string napravlenie2 = "←";
-            string napravlenie3 = "↑";
-            string napravlenie4 = "↓";
-            switch (napravlenie, napravlenie2, napravlenie3, napravlenie4)
+            ConsoleKeyInfo direction = Console.ReadKey();
+            go = go + 1;
+            buffer_B= buffer_B + 1;
+            switch(direction.Key)
             {
-                
-
+                case ConsoleKey.W:
+                    {
+                        x = x - 1;
+                        break;
+                    }
+                case ConsoleKey.D:
+                    {
+                        y = y + 1;
+                        break;
+                    }
+                case ConsoleKey.A:
+                    {
+                        y = y - 1;
+                        break;
+                    }
+                case ConsoleKey.S:
+                    {
+                        x = x + 1;
+                        break;
+                    }
             }
+            return (carta);
         }
-        public static void Fight()
+        public static Array Fight()
         {//обмен ударами игрока и врага до полной потери здоровья одним из них}
+            vrag_HP = 15;
+            for(int i = 0; igrok_HP !=0 & vrag_HP!=0; i++)
+            {
+                vrag_HP = vrag_HP - igrok_power;
+                if(vrag_HP <= 0)
+                {
+                    Console.WriteLine("Враг побежден!");
+                    break;
+                }
+                igrok_HP = igrok_HP - vrag_power;
+                if (igrok_HP == 0 | igrok_HP<0)
+                {
+                    Console.WriteLine("Вы умерли!");
+                }
+            }
+            kolvo_vrag = kolvo_vrag - 1;
+            return (carta);
         }
         public static void Healing()
         {// лечение}
+            igrok_HP = 30;
         }
         public static void Buff()
         {//увеличение силы}
+            buffer_B = 0;
+            igrok_power = 10;
         }
         public static void SaveState()
         {//сохранение в файл}
         }
     }
 }
+    
+
 
