3f35121490e215588546c82b3bdc6e4328fc4f78
MMMRita13 <114126777+MMMRita13@users.noreply.github.com>
2023-01-19 11:22:31 +0500

    Add files via upload

---
+++ рz_20/рz_20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace рz_20
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char[] massiv = s.ToCharArray();
+            for (int i = 0; i < massiv.Length; i++)
+            {
+                massiv[i] = char.IsLower(massiv[i]) ? char.ToUpper(massiv[i]) : char.ToLower(massiv[i]);
+
+            }
+            Console.WriteLine(massiv);
+        }
+     }
+}
