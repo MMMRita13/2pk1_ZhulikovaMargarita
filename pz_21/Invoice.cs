ed838d2a3a526cae3e90ee771b3174eb1ea2b0d5
MMMRita13 <114126777+MMMRita13@users.noreply.github.com>
2023-02-04 11:25:02 +0500

    Add files via upload

---
+++ pz_21/Invoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace pz_21
+{
+    class Invoice
+    {
+        public Guid Id;
+        public DateTime date;
+        public string product;
+        public int count;
+        public int price;
+        public float NDS;
+    }
+}
---
+++ pz_21/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21
+{
+    
+ 
+    class Program
+    {
+        static Invoice PrintInfo() //Выводит общее описание накладной со всеми значениями
+        {
+            Invoice invoice = new Invoice();
+
+            invoice.Id = Guid.NewGuid();
+            invoice.date = new DateTime(2015, 7, 20); 
+            invoice.product = "Гречка";
+            invoice.count = 40;
+            invoice.price = 100;
+            invoice.NDS= 0.1f;
+
+            return invoice;
+        }
+
+        static void GetFullPrice(Invoice invoice)// Возвращает полную стоимость всего товара по накладной с учетом НДС
+        {
+            Console.WriteLine("Информация о счете в приложении");
+            Console.WriteLine($"ID: {invoice.Id}");
+            Console.WriteLine($"Дата проведения накладной: {invoice.date}");
+            Console.WriteLine($"Наименование товара: {invoice.product}");
+            Console.WriteLine($"Количество единиц товара: {invoice.count}");
+            Console.WriteLine($"Стоимость единицы товара: {invoice.price}");
+            Console.WriteLine($"Стоимость с учетом НДС: {invoice.price*(1+invoice.NDS)}");
+
+        }
+        static void Main(string[] args)
+        {
+
+            var product = PrintInfo();
+            GetFullPrice(product);
+        }
+    }
+}
