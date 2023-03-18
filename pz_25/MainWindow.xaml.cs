7136b8fbee68de574cb2ad80d53a20f33fc12687
MMMRita13 <114126777+MMMRita13@users.noreply.github.com>
2023-03-18 09:50:40 +0500

    pz

--- pz_25/MainWindow.xaml.cs
+++ pz_25/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,10 +25,107 @@ namespace pz_25
         {
             InitializeComponent();
         }
+        double a, b;
+        
 
         private void press8Button_Click(object sender, RoutedEventArgs e)
         {
-
+            infoTextBlock.Text += "8";
         }
+
+        private void press7Button_Click(object sender, RoutedEventArgs e)
+        {
+            infoTextBlock.Text += "7";
+        }
+
+        private void press9Button_Click(object sender, RoutedEventArgs e)
+        {
+            infoTextBlock.Text += "9";
+        }
+
+        private void press6Button_Click(object sender, RoutedEventArgs e)
+        {
+            infoTextBlock.Text += "6";
+        }
+
+        private void press5Button_Click(object sender, RoutedEventArgs e)
+        {
+            infoTextBlock.Text += "5";
+        }
+
+        private void press4Button_Click(object sender, RoutedEventArgs e)
+        {
+            infoTextBlock.Text += "4";
+        }
+
+        private void press3Button_Click(object sender, RoutedEventArgs e)
+        {
+            infoTextBlock.Text += "3";
+        }
+
+        private void press2Button_Click(object sender, RoutedEventArgs e)
+        {
+            infoTextBlock.Text += "2";
+        }
+
+        private void press1Button_Click(object sender, RoutedEventArgs e)
+        {
+            infoTextBlock.Text += "1";
+        }
+
+        private void press0Button_Click(object sender, RoutedEventArgs e)
+        {
+            infoTextBlock.Text += "0";
+        }
+
+        private void pressplusButton_Click(object sender, RoutedEventArgs e)
+        {
+            a = double.Parse(infoTextBlock.Text);  
+            infoTextBlock.Text += "+";
+        }
+
+        private void pressminusButton_Click(object sender, RoutedEventArgs e)
+        {
+            a = double.Parse(infoTextBlock.Text);
+            infoTextBlock.Text += "-";
+          
+        }
+
+        private void pressdelButton_Click(object sender, RoutedEventArgs e)
+        {
+            a = double.Parse(infoTextBlock.Text);
+            infoTextBlock.Text += "/";
+        }
+        private void pressymnozhButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            a = double.Parse(infoTextBlock.Text);
+            infoTextBlock.Text += "*";
+        }
+
+        private void pressravnoButton_Click(object sender, RoutedEventArgs e)
+        {
+            string expression = infoTextBlock.Text;
+            infoTextBlock.Text = Calculator(expression).ToString();
+        }
+        private double Calculator(string expression)
+        {
+            //double c = Convert.ToDouble(expression);
+            string pattern = @"\d";
+            Regex regex= new Regex(pattern);
+            switch (Regex.IsMatch(infoTextBlock.Text, pattern))
+            {
+                case true:
+                    
+                    break;
+                case false:
+                    
+                    
+                    break;
+                
+            }
+            return 0;
+        }
+
+       
     }
 }
---
+++ pz_26.1/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pz_26._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Действие выполнено");
+        }
+        private void escButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(); // закрытие окна
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void acceptButton2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        static void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            FileStream file1 = new FileStream("C:\\Users\\Маргарита\\2pk1_ZhulikovaMargarita\\pz_26.1\\data\\", FileMode.Create); //создание файла
+            StreamWriter writer1 = new StreamWriter(file1);
+            writer1.Write("");
+            writer1.Close();
+            FileStream file2 = new FileStream("C:\\Users\\Маргарита\\2pk1_ZhulikovaMargarita\\pz_26.1\\data\\", FileMode.Open); //открытие файла
+            StreamWriter writer2 = new StreamWriter(file2);
+            writer2.Write("");
+            writer2.Close();
+
+        }
+        private int GetLineIndexFromCharacterIndex()
+        {
+            int CaretIndex = 0;
+            return CaretIndex;
+        }
+        private void UpdateCursorPosition(object textField)
+        {
+            //int row = textField.GetLineIndexFromCharacterIndex(textField.CaretIndex);
+            //int col = textField.CaretIndex - textField.GetLineIndexFromCharacterIndex(row);
+            //cursorPosition.Text = $"строка: {row + 1} столбец: {col + 1}";
+        }
+
+
+    }
+}
